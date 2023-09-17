bbe42d53fb91d0e2e2043c4bef070ebc030a3ec1
yopisaputra <yopisaputra9@gmail,com>
2023-09-17 11:16:34 +0700

    Tambah advance python

---
+++ advancePython/functionParameter.py
@@ -0,0 +1,16 @@
+"""
+FUNCTION
+    tanpa parameter 
+        def namaFungsi():
+        
+    dengan argumen/parameter 
+        def namaFungsi(argumen):
+        
+    parameter default 
+        def namaFungsi(argumen = 'default'):
+"""
+
+def hitungPython(a = 0, b = 0):
+    print("jumlah dan b adalah", a + b)
+
+hitungPython(5,5)
---
+++ advancePython/returnFunction.py
@@ -0,0 +1,17 @@
+"""
+FUNCTIONS
+    Parameter
+    Return
+"""
+
+def hitung(a,b):
+    c = a+b
+    return c
+
+hasil1 = (hitung(10, 20))+50
+
+hasil2 = hitung(hasil1, 400)
+
+print("text..",hasil2)
+
+
